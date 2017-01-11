18678136b20934ee5e37d136b26e3970caa81336
lupt <lupt@luptdeMacBook-Pro.local>
2017-01-11 23:46:01 +0800

    页面及样式修改

--- static/js/common.js
+++ static/js/common.js
@@ -4,30 +4,20 @@
 $(function () {
 /*    $(".nav_bar a").on("click", function () {
         $(this).addClass("on").parents("li").siblings("li").find("a").removeClass("on");
-        $(this).children("i").removeClass("icon_nav01").addClass("icon_nav02").parents("li").siblings("li").find("i").removeClass("icon_nav02").addClass("icon_nav01")
-        });*/
-    $(".nav_bar a").each(function(){
-        $this = $(this);
-        if($(this)[0].href==String(window.location)){
-            $(this).addClass("on").parents("li").siblings("li").find("a").removeClass("on");
-            $(this).children("i").removeClass("icon_nav01").addClass("icon_nav02").parents("li").siblings("li").find("i").removeClass("icon_nav02").addClass("icon_nav01")
-        }
+        $(this).children("i").removeClass("icon_nav01").addClass("icon_nav02").parents("li").siblings("li").find("i").removeClass("icon_nav02").addClass                ("icon_nav01")
     });
-
     $(".dashboard_tab > li").on("click",function(){
         $(this).find("span").addClass("blue").end().siblings("li").find("span").removeClass("blue")
-    });
+    });*/
    /* 左侧导航*/
     $(".left_nav li").on("click",function(){
         $(this).find("h5").addClass("in").end().siblings("li").find("h5").removeClass("in");
         $(this).find("dl").slideDown().end().siblings("li").find("dl").slideUp()
     });
-
     $(".left_nav li dl dd").on("click",function(){
         $(this).find("a").addClass("ck").end().siblings("dd").find("a").removeClass("ck");
         $(this).parents("li").siblings("li").find("a").removeClass("ck")
     });
-
     <!--点击投票按钮弹出层-->
     $(".vote_before").on('click',function(){
         $(this).parents(".vote_item").find(".vote_tip").show()
@@ -36,9 +26,15 @@ $(function () {
         $('.vote_tip').hide()
     });
 
-    $('.left_nav h5.in').siblings('dl').show();
-
-    /*交易所表*/
-
+   // 选择语言
+$('#language').on('click',function(){
+    $('.language_list,.pointer').show()
+});
+$('.language_list li').on('click',function(){
+    var $text =$(this).text();
+    $('#language span').text($text);
+    $('.language_list,.pointer').hide()
+})
+    
 });
 
